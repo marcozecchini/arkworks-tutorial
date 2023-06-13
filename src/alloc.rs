1dd7b582245a474c202f0fe6678121956cfb2ed9
marcozecchini <zecchini@diag.uniroma1.it>
2023-06-13 15:40:47 +0200

    example finished

--- src/alloc.rs
+++ src/alloc.rs
@@ -1,6 +1,5 @@
 use std::borrow::Borrow;
-
-use ark_ff::PrimeField;
+use ark_ff::{PrimeField};
 use ark_r1cs_std::{prelude::{AllocVar, AllocationMode}, uint8::UInt8};
 use ark_relations::r1cs::{Namespace, SynthesisError};
 
@@ -14,7 +13,7 @@ impl<const N: usize, F: PrimeField> AllocVar<[[u8; N]; N], F> for Puzzle<N, F> {
     ) -> Result<Self, SynthesisError> {
         let cs = cs.into();
         let row = [(); N].map(|_| UInt8::constant(0));
-        let mut puzzle = Puzzle([(); N].map(|_| row.clone()));
+        let mut puzzle = Puzzle([(); N].map(|_| row.clone())); // In this way I init the puzzle
         let value = f().map_or([[0; N]; N], |f| *f.borrow());
         for (i, row) in value.into_iter().enumerate() {
             for (j, cell) in row.into_iter().enumerate() {
